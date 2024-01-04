1440b10f785a940a58cbcb870b54b9ac35dcfadd
E-SeongHo <lsh031255@naver.com>
2024-01-04 17:01:13 +0900

    imports

---
+++ Assets/Imports/MazeGenerator/Scripts/MazeSpawner.cs
@@ -0,0 +1,135 @@
+﻿using UnityEngine;
+using System.Collections;
+
+//<summary>
+//Game object, that creates maze and instantiates it in scene
+//</summary>
+public class MazeSpawner : MonoBehaviour {
+	public enum MazeGenerationAlgorithm{
+		PureRecursive,
+		RecursiveTree,
+		RandomTree,
+		OldestTree,
+		RecursiveDivision,
+	}
+
+	public MazeGenerationAlgorithm Algorithm = MazeGenerationAlgorithm.PureRecursive;
+	public bool FullRandom = false;
+	public int RandomSeed = 12345;
+	public GameObject Floor = null;
+	public GameObject Wall = null;
+	public GameObject Pillar = null;
+	public int Rows = 5;
+	public int Columns = 5;
+	public float CellWidth = 5;
+	public float CellHeight = 5;
+	public bool AddGaps = true;
+	public GameObject GoalPrefab = null;
+	public GameObject ExitPrefab = null;
+
+	private BasicMazeGenerator mMazeGenerator = null;
+
+	//(Add) 
+	public GameObject[,] floorObjects = null;
+	public MazeCell[,] GetMazeInfo()
+    {
+		return mMazeGenerator.Maze;
+	}
+
+	void Start () {
+		if (!FullRandom) {
+			Random.seed = RandomSeed;
+		}
+		switch (Algorithm) {
+		case MazeGenerationAlgorithm.PureRecursive:
+			mMazeGenerator = new RecursiveMazeGenerator (Rows, Columns);
+			break;
+		case MazeGenerationAlgorithm.RecursiveTree:
+			mMazeGenerator = new RecursiveTreeMazeGenerator (Rows, Columns);
+			break;
+		case MazeGenerationAlgorithm.RandomTree:
+			mMazeGenerator = new RandomTreeMazeGenerator (Rows, Columns);
+			break;
+		case MazeGenerationAlgorithm.OldestTree:
+			mMazeGenerator = new OldestTreeMazeGenerator (Rows, Columns);
+			break;
+		case MazeGenerationAlgorithm.RecursiveDivision:
+			mMazeGenerator = new DivisionMazeGenerator (Rows, Columns);
+			break;
+		}
+		mMazeGenerator.GenerateMaze ();
+		// (Add)
+		floorObjects = new GameObject[Rows, Columns];
+
+		for (int row = 0; row < Rows; row++) {
+			for(int column = 0; column < Columns; column++){
+				float x = column*(CellWidth+(AddGaps?.2f:0));
+				float z = row*(CellHeight+(AddGaps?.2f:0));
+				MazeCell cell = mMazeGenerator.GetMazeCell(row,column);
+				GameObject tmp;
+				tmp = Instantiate(Floor,new Vector3(x,0,z), Quaternion.Euler(0,0,0)) as GameObject;
+				tmp.transform.parent = transform;
+				floorObjects[row, column] = tmp;
+
+                // (Add) for tracking visit this cell
+                VisitTracker traker = tmp.gameObject.GetComponent<VisitTracker>();
+                traker.SetRowColumn(row, column);
+
+                // (Add) for determine rotation angle of poneglyph
+                bool[] isOpen = { true, true, true, true }; 
+
+				if(cell.WallRight){
+					isOpen[0] = false;
+					tmp = Instantiate(Wall,new Vector3(x+CellWidth/2,0,z)+Wall.transform.position,Quaternion.Euler(0,90,0)) as GameObject;// right
+					tmp.transform.parent = transform;
+				}
+				if(cell.WallFront){
+					isOpen[1] = false;
+					tmp = Instantiate(Wall,new Vector3(x,0,z+CellHeight/2)+Wall.transform.position,Quaternion.Euler(0,0,0)) as GameObject;// front
+					tmp.transform.parent = transform;
+				}
+				if(cell.WallLeft){
+					isOpen[2] = false;
+					tmp = Instantiate(Wall,new Vector3(x-CellWidth/2,0,z)+Wall.transform.position,Quaternion.Euler(0,270,0)) as GameObject;// left
+					tmp.transform.parent = transform;
+				}
+				if(cell.WallBack){
+					isOpen[3] = false;
+					tmp = Instantiate(Wall,new Vector3(x,0,z-CellHeight/2)+Wall.transform.position,Quaternion.Euler(0,180,0)) as GameObject;// back
+					tmp.transform.parent = transform;
+				}
+				if(row==0 && column == 0) { }
+				// (Add) create My Exit button here
+				if (cell.IsExit)
+                {
+					tmp = Instantiate(ExitPrefab, new Vector3(x, 1, z), Quaternion.Euler(0, 0, 0)) as GameObject;
+					tmp.transform.parent = transform;
+				}
+				else if (cell.IsGoal && GoalPrefab != null)
+				{
+					int rotationIndex = 0;
+					for (int i = 0; i < 4; i++)
+						if (isOpen[i]) rotationIndex = i;
+
+					float rotation = (rotationIndex + 1) * 90.0f;
+					tmp = Instantiate(GoalPrefab, new Vector3(x, 2.5f, z), Quaternion.Euler(0, 0, 0)) as GameObject;
+					tmp.transform.parent = transform;
+					tmp.transform.rotation = Quaternion.Euler(0, -rotation, 0);
+				}
+			}
+		}
+
+		if(Pillar != null){
+			for (int row = 0; row < Rows+1; row++) {
+				for (int column = 0; column < Columns+1; column++) {
+					float x = column*(CellWidth+(AddGaps?.2f:0));
+					float z = row*(CellHeight+(AddGaps?.2f:0));
+					GameObject tmp = Instantiate(Pillar,new Vector3(x-CellWidth/2,0,z-CellHeight/2),Quaternion.identity) as GameObject;
+					tmp.transform.parent = transform;
+				}
+			}
+		}
+		// (Add) for my feature
+		gameObject.GetComponent<PathFinder>().enabled = true;
+	}
+}
